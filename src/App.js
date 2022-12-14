2e6589c35734c75399a3ac6dee5027b7f795a541
SerheyBernatovich <sec1982@ukr.net>
2022-12-14 09:32:53 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Куринная" price={400} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
