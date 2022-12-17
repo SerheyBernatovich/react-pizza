4d885c78cb1b9dced5dd548489c5eb9eba33f77e
SerheyBernatovich <sec1982@ukr.net>
2022-12-17 11:10:16 +0200

    add

---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFoundBlock = () => {
+  return (
+    <div>
+      <h1>
+        <span>😕</span>
+        <br />
+        Not found
+      </h1>
+    </div>
+  );
+};
+
+export default NotFoundBlock;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://639c590f16d1763ab14707cf.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};
+export default Home;
