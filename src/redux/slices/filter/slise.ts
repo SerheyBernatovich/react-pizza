ea3d6987a59c23032edc0046fadfa70715c8edc2
SerheyBernatovich <sec1982@ukr.net>
2023-01-14 09:58:52 +0200

    add

--- src/redux/slices/filter/slise.ts
+++ src/redux/slices/filter/slise.ts
@@ -1,26 +1,5 @@
+import { FilterSliceState, Sort, SortPropertyEnum } from './types';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from '../stor';
-
-export enum SortPropertyEnum {
-  RATING_DESC = 'rating',
-  RATING_ASC = '-rating',
-  TITLE_DESC = 'title',
-  TITLE_ASC = '-title',
-  PRICE_DESC = 'price',
-  PRICE_ASC = '-price',
-}
-
-export type Sort = {
-  name: string;
-  sortProperty: SortPropertyEnum;
-};
-
-export interface FilterSliceState {
-  searchValue: string;
-  categoryId: number;
-  currentPage: number;
-  sort: Sort;
-}
 
 const initialState: FilterSliceState = {
   searchValue: '',
@@ -65,8 +44,8 @@ const filterSlice = createSlice({
   },
 });
 
-export const selectFilter = (state: RootState) => state.filter;
-export const selectSort = (state: RootState) => state.filter.sort;
+// export const selectFilter = (state: RootState) => state.filter;
+// export const selectSort = (state: RootState) => state.filter.sort;
 
 export const {
   setSearchValue,
